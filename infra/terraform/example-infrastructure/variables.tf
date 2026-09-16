8cf0f9c1b019d63bbea750dd28443ed82d846426
heyohjayy <gojex.og@gmail.com>
2026-09-16 01:34:34 +0100

    refactor: generalize example infrastructure variables

--- infra/terraform/example-infrastructure/variables.tf
+++ infra/terraform/example-infrastructure/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" { type = string }
 variable "vpc_id" { type = string }
 variable "artifacts_bucket_name" { type = string }
-variable "enable_sprint3_route53" {
+variable "enable_validation_route53" {
   type    = bool
   default = false
 }
-variable "sprint3_route53_zone_name" {
+variable "validation_route53_zone_name" {
   type    = string
-  default = "driftctl-sprint3.invalid"
+  default = "driftctl-validation.invalid"
 }
